67cbd849a2f022b66d56590fae3ac9e3dd5853f4
AmpiroMax <patrat2015@yandex.ru>
2022-09-23 16:47:07 +0300

    Task 2 and 3 were copied from prev year

--- 01_first_images/homework/task_2.py
+++ 01_first_images/homework/task_2.py
@@ -2,16 +2,79 @@ import cv2
 import numpy as np
 
 
-def find_road_number(image: np.ndarray) -> int:
-    """
-    Найти номер дороги, на которой нет препятсвия в конце пути.
+def find_road_number(image):
+    image_hsv = cv2.cvtColor(image.copy(), cv2.COLOR_RGB2HSV)
 
-    :param image: исходное изображение
-    :return: номер дороги, на котором нет препятсвия на дороге
-    """
-    road_number = None
-    # Ваш код тут
-    pass
-    # Ваш код тут
+    # подбираем маски
 
-    return road_number
+    # маска препятствий
+    hsv_low = (1, 250, 250)
+    hsv_high = (255, 255, 255)
+    obstacle = cv2.inRange(image_hsv, hsv_low, hsv_high)
+
+    # маска на машину
+    hsv_low = (109, 0, 0)
+    hsv_high = (112, 255, 255)
+    car = cv2.inRange(image_hsv, hsv_low, hsv_high)
+
+    # маска на поолосы
+    hsv_low = (25, 0, 0)
+    hsv_high = (32, 255, 255)
+    lines = cv2.inRange(image_hsv, hsv_low, hsv_high)
+
+    # дополнительная маска на костомизацию
+    hsv_low = (1, 250, 250)
+    hsv_high = (255, 255, 255)
+    additional_mask = cv2.inRange(image_hsv, hsv_low, hsv_high)
+
+    # несмотря на все маски получить ровную и аккуратную картинку не удалось
+    # masked_image
+    mimage = additional_mask + lines + car + obstacle
+
+    h, w = mimage.shape
+    line_size = 0
+    road_size = 0
+    for i in range(w):
+        if(mimage[h//2, i] != 0 and road_size != 0):
+            break
+        if(mimage[h//2, i] != 0):
+            line_size += 1
+        if(mimage[h//2, i] == 0 and line_size != 0):
+            road_size += 1
+
+    road_count = round((h - line_size) / (line_size + road_size))
+    roads = np.zeros(road_count)
+
+    # определяем есть на дороге препятствие или нет
+    delta = int(((w - line_size) / road_count) // 2)
+    for road_num in range(road_count):
+        for dh in range(h // 2):
+            # у меня почему-то после маски картинка не бинарной получилась
+            if(mimage[dh, delta + 2 * delta * road_num] == 255 or mimage[dh, delta + 2 * delta * road_num] == 254):
+                roads[road_num] = -1
+                break
+
+    # определяем есть на дороге машина или нет
+    cars = np.zeros(road_count)
+    for road_num in range(road_count):
+        for dh in range(h // 2, h, 1):
+            if(mimage[dh, delta + 2 * delta * road_num] == 255 or mimage[dh, delta + 2 * delta * road_num] == 254):
+                cars[road_num] = -1
+                break
+
+    curr_num = 0
+    num = 0
+    for i in range(road_count):
+        if(roads[i] == 0):
+            num = i
+        if(cars[i] == -1):
+            curr_num = i
+
+    if(curr_num == num):
+        print("Перестраиваться не нужно")
+
+    print(roads)
+    print(cars)
+    # plot_one_image(mimage)
+
+    return num+1
--- 01_first_images/homework/task_3.py
+++ 01_first_images/homework/task_3.py
@@ -11,10 +11,17 @@ def rotate(image, point: tuple, angle: float) -> np.ndarray:
     :param angle: угол поворота
     :return: повернутное изображение
     """
-    # Ваш код
-    pass
+    h, w, _ = image.shape
 
-    return image
+    # для поворота я использую аффинкук реализованную ниже
+    # определяем куда перейдут точки после аффинки
+    pts1 = np.float32([[w/2, 0], [w, h / 2], [w/2, h/2]])
+    angle = angle * np.pi / 180
+    pts2 = np.float32([[w/2 - h/2 * np.sin(angle), h/2 - h/2 * np.cos(angle)],
+                      [w/2 + w/2 * np.cos(angle), h/2 - w/2 * np.sin(angle)], [w/2, h/2]])
+
+    # возвращаем результат аффинного преобразоваания
+    return apply_warpAffine(image, pts1, pts2)
 
 
 def apply_warpAffine(image, points1, points2) -> np.ndarray:
@@ -27,7 +34,45 @@ def apply_warpAffine(image, points1, points2) -> np.ndarray:
     :param points2:
     :return: преобразованное изображение
     """
-    # Ваш код
-    pass
+    h, w, _ = image.shape
+    picture = image.copy()
+    M = cv2.getAffineTransform(points1, points2)
 
-    return image
+    x1 = 0
+    y1 = 0
+
+    x2 = w
+    y2 = h
+
+    # координаты 4 начальных точек
+    dot1 = [x1, y1, 1]
+    dot2 = [x1, y2, 1]
+    dot3 = [x2, y1, 1]
+    dot4 = [x2, y2, 1]
+
+    # координаты точек после преобразования
+    dot11 = M @ dot1
+    dot21 = M @ dot2
+    dot31 = M @ dot3
+    dot41 = M @ dot4
+
+    # размеры окна в котором будем отрисовывать картинку
+    w1 = max(abs(dot41[0] - dot11[0]), abs(dot31[0] - dot21[0]))
+    h1 = max(abs(dot41[1] - dot11[1]), abs(dot31[1] - dot21[1]))
+
+    # сдвиг в начало координат
+    dx = -min(dot11[0], dot21[0], dot31[0], dot41[0])
+    dy = -min(dot11[1], dot21[1], dot31[1], dot41[1])
+    Move = np.float32([[1, 0, dx], [0, 1, dy], [0, 0, 1]])
+
+    # я использовал матричные преобразования, поэтому добавляю
+    # ещё одну строчку, чтобы можно было перемножить матрицы
+    M1 = list(M)
+    M1 += [[0, 0, 1]]
+
+    # получаем итоговую матрицу преобразования
+    Matr = Move @ M1
+    Matr = Matr[:-1]
+
+    picture = cv2.warpAffine(picture, Matr, (int(w1), int(h1)))
+    return picture
