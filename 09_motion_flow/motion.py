cc44ed83c20342b0a3a27d59e9b412a8a478fc0e
AmpiroMax <patrat2015@yandex.ru>
2022-11-25 22:31:43 +0300

    HW6 was done

--- 09_motion_flow/motion.py
+++ 09_motion_flow/motion.py
@@ -7,14 +7,18 @@ import numpy as np
 from tqdm.auto import tqdm
 
 
-def create_video(save_dir, size, img_format='jpg', vido_format='avi'):
+def create_video(save_dir, size, img_format="jpg", vido_format="avi"):
     out_name = Path(save_dir).parts[-1]
 
-    fourcc = cv2.VideoWriter_fourcc(*'mp4v')
-    out = cv2.VideoWriter(str(Path(save_dir) / Path(f'{out_name}.{vido_format}')),
-                          fourcc, 20, tuple(size.astype(int)))
+    fourcc = cv2.VideoWriter_fourcc(*"mp4v")
+    out = cv2.VideoWriter(
+        str(Path(save_dir) / Path(f"{out_name}.{vido_format}")),
+        fourcc,
+        20,
+        tuple(size.astype(int)),
+    )
 
-    for fname in tqdm(sorted(map(str, Path(save_dir).glob(f'*.{img_format}')))):
+    for fname in tqdm(sorted(map(str, Path(save_dir).glob(f"*.{img_format}")))):
         imag = skimage.io.imread(fname)
         out.write(imag)
     out.release()
@@ -35,19 +39,29 @@ def sparse_motion():
     # Parameters for Shi-Tomasi corner detection
     feature_params = dict(maxCorners=300, qualityLevel=0.2, minDistance=2, blockSize=7)
     # Parameters for Lucas-Kanade optical flow
-    lk_params = dict(winSize=(15, 15), maxLevel=2, criteria=(cv2.TERM_CRITERIA_EPS | cv2.TERM_CRITERIA_COUNT, 10, 0.03))
+    lk_params = dict(
+        winSize=(15, 15),
+        maxLevel=2,
+        criteria=(cv2.TERM_CRITERIA_EPS | cv2.TERM_CRITERIA_COUNT, 10, 0.03),
+    )
 
     cap = cv2.VideoCapture("shibuya.mp4")
     get_video_details(cap)
     cv2.startWindowThread()
-    cv2.namedWindow('imageWindow')
+    cv2.namedWindow("imageWindow")
     is_ok, first_frame = cap.read()
+
+    if not is_ok:
+        print("Not OK")
+
+    if first_frame is None:
+        print("None image was given")
 
     prev_gray = cv2.cvtColor(first_frame, cv2.COLOR_BGR2GRAY)
     prev = cv2.goodFeaturesToTrack(prev_gray, mask=None, **feature_params)
     mask = np.zeros_like(first_frame)
     idx = 0
-    while (cap.isOpened()):
+    while cap.isOpened():
         is_ok, frame = cap.read()
         if not is_ok:
             break
@@ -55,7 +69,9 @@ def sparse_motion():
         mask = np.zeros_like(frame)
 
         prev = cv2.goodFeaturesToTrack(prev_gray, mask=None, **feature_params)
-        next, status, error = cv2.calcOpticalFlowPyrLK(prev_gray, gray, prev, None, **lk_params)
+        next, status, error = cv2.calcOpticalFlowPyrLK(
+            prev_gray, gray, prev, None, **lk_params
+        )
 
         good_old = prev[status == 1].astype(int)
         good_new = next[status == 1].astype(int)
@@ -74,9 +90,9 @@ def sparse_motion():
 
         # Opens a new window and displays the output frame
         cv2.imshow("sparse optical flow", output)
-        cv2.imwrite(f'tmp/frame_{idx}.jpg', output)
+        cv2.imwrite(f"tmp/frame_{idx}.jpg", output)
         idx += 1
-        if cv2.waitKey(1) & 0xFF == ord('q'):
+        if cv2.waitKey(1) & 0xFF == ord("q"):
             break
 
     # The following frees up resources and closes all windows
@@ -89,15 +105,21 @@ def dense_motion():
     cap = cv2.VideoCapture(0)
     is_ok, first_frame = cap.read()
 
-    farn_params = dict(pyr_scale=0.5, levels=3, winsize=35,
-                       iterations=3, poly_n=5, poly_sigma=1.2,
-                       flags=cv2.OPTFLOW_FARNEBACK_GAUSSIAN)
+    farn_params = dict(
+        pyr_scale=0.5,
+        levels=3,
+        winsize=35,
+        iterations=3,
+        poly_n=5,
+        poly_sigma=1.2,
+        flags=cv2.OPTFLOW_FARNEBACK_GAUSSIAN,
+    )
 
     prev_gray = cv2.cvtColor(first_frame, cv2.COLOR_BGR2GRAY)
     mask = np.zeros_like(first_frame)
     mask[..., 1] = 255
 
-    while (cap.isOpened()):
+    while cap.isOpened():
         is_ok, frame = cap.read()
         if not is_ok:
             break
@@ -116,14 +138,14 @@ def dense_motion():
 
         prev_gray = gray
 
-        if cv2.waitKey(1) & 0xFF == ord('q'):
+        if cv2.waitKey(1) & 0xFF == ord("q"):
             break
     # The following frees up resources and closes all windows
     cap.release()
     cv2.destroyAllWindows()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     sparse_motion()
     # dense_motion()
 
